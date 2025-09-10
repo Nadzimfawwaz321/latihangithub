7b78e0e9698ebf1149ab0dd5dbb257138492f64b
Nadzimfawwaz321 <e42241971@student.polije.ac.id>
2025-09-10 14:56:09 +0700

    latihangithub

---
+++ belajar.js
@@ -0,0 +1,9 @@
+alert("Hallo gok");
+confirm("Apakah kamu ingin melanjutkan?");
+prompt("Masukkan nama kamu:","Mohammad Nadzim F");
+function foo() { 
+alert("Ini hanyalah belajar jangan dibuat serius");
+} foo(); a 
+Date("2024-06-12");
+parseInt("2");
+setTimeout(function(){ alert("Ini adalah alert dari setTimeout"); }, 3000);
